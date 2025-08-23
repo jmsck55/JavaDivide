ed1a867d3c69e54064e24795eaa58d60a24e3784
James Cook <jmsck55@gmail.com>
2025-08-23 01:31:05 -0400

    Update Divide.java
    Changed 45 to 55.

--- src/Divide/Divide.java
+++ src/Divide/Divide.java
@@ -12,7 +12,7 @@ public class Divide {
         double c, q, r;
         q = 1.0 / d;
         r = 0;
-        for (int i = 0; i <= 45; i++) { // 45
+        for (int i = 0; i <= 55; i++) { // 55
           c = q * (2 - q * d);
           if (c == q) {
             break;
